1719609575a814653f776c386ed19d11b5723eae
MayurArchives <mayurkanyal10@gmail.com>
2024-12-03 21:33:14 +0530

    Print All Subarrays

---
+++ day10_ques4.java
@@ -0,0 +1,26 @@
+import java.io.*;
+import java.util.*;
+
+public class day10_ques4 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int arr[] = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        for (int i = 0; i < n; i++) {
+            for (int j = i; j < n; j++) {
+                for (int k = i; k <= j; k++) {
+                    System.out.print(arr[k] + " ");
+                }
+                System.out.println();
+            }
+        }
+        /*
+         * Enter your code here. Read input from STDIN. Print output to STDOUT. Your
+         * class should be named Solution.
+         */
+    }
+}
