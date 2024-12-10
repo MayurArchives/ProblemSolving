5add2ef6696007895dec39a264418fcd7e1af365
MayurArchives <mayurkanyal10@gmail.com>
2024-12-10 10:08:54 +0530

    Zeroes and Ones

--- day13_ques5.java
+++ day13_ques5.java
@@ -1,3 +1,38 @@
+import java.io.*;
+import java.util.*;
+
 public class day13_ques5 {
 
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int arr[] = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        int left = 0;
+        int right = n - 1;
+        while (left < right) {
+            if (arr[left] == 1 && arr[right] == 0) {
+                int temp = arr[left];
+                arr[left] = arr[right];
+                arr[right] = temp;
+                left++;
+                right--;
+            }
+            if (arr[left] == 0) {
+                left++;
+            }
+            if (arr[right] == 1) {
+                right--;
+            }
+        }
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+        /*
+         * Enter your code here. Read input from STDIN. Print output to STDOUT. Your
+         * class should be named Solution.
+         */
+    }
 }
