fafee9a8ba4d2d48fc1df04c3c624fd4967029e6
MayurArchives <mayurkanyal10@gmail.com>
2024-12-10 10:07:40 +0530

    Rotate Right

---
+++ day13_ques4.java
@@ -0,0 +1,37 @@
+import java.io.*;
+import java.util.*;
+
+public class day13_ques4 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int arr[] = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        int k = sc.nextInt();
+        k = k % n;
+        reverse(arr, 0, n - 1);
+        reverse(arr, 0, k - 1);
+        reverse(arr, k, n - 1);
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+
+        /*
+         * Enter your code here. Read input from STDIN. Print output to STDOUT. Your
+         * class should be named Solution.
+         */
+    }
+
+    public static void reverse(int arr[], int left, int right) {
+        while (left < right) {
+            int temp = arr[left];
+            arr[left] = arr[right];
+            arr[right] = temp;
+            left++;
+            right--;
+        }
+    }
+}
