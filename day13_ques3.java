5a1436a57c990857d98e701ba4f40011bd83a6ba
MayurArchives <mayurkanyal10@gmail.com>
2024-12-10 10:06:02 +0530

    HW_Sum of All Odd Length Subarrays 1

--- day13_ques3.java
+++ day13_ques3.java
@@ -1,3 +1,28 @@
+import java.io.*;
+import java.util.*;
+
 public class day13_ques3 {
 
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int arr1[] = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr1[i] = sc.nextInt();
+        }
+        int totalsum = 0;
+        for (int i = 0; i < n; i++) {
+            int start = i + 1;
+            int end = n - i;
+            int totalsubarray = start * end;
+            int oddsubarray = (totalsubarray + 1) / 2;
+            totalsum += arr1[i] * oddsubarray;
+
+        }
+        System.out.println(totalsum);
+        /*
+         * Enter your code here. Read input from STDIN. Print output to STDOUT. Your
+         * class should be named Solution.
+         */
+    }
 }
