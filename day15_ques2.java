631d1f826338a063621478f92f573aae5a60f724
MayurArchives <mayurkanyal10@gmail.com>
2024-12-12 21:14:40 +0530

    Print string are equal or not

--- day15_ques2.java
+++ day15_ques2.java
@@ -1,3 +1,17 @@
+import java.util.Scanner;
+
 public class day15_ques2 {
-    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str1 = sc.nextLine();
+        String str2 = sc.nextLine();
+
+        if (str1.equals(str2)) {
+            System.out.println("equals");
+        } else {
+            System.out.println("not equals");
+        }
+
+    }
+
 }
