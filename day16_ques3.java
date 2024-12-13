62eedd5fb15e6992473f300656006c53104506b0
MayurArchives <mayurkanyal10@gmail.com>
2024-12-13 19:31:07 +0530

    Int with Maximum Freq

--- day16_ques3.java
+++ day16_ques3.java
@@ -1,3 +1,28 @@
+import java.io.*;
+import java.util.*;
+
 public class day16_ques3 {
 
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] freq = new int[10]; // Array to store frequency of digits 0-9
+        for (int i = 0; i < n; i++) {
+            int num = sc.nextInt();
+            freq[num]++;
+        }
+        int maxFreq = 0;
+        int result = 0;
+        for (int i = 0; i < 10; i++) {
+            if (freq[i] > maxFreq) {
+                maxFreq = freq[i];
+                result = i;
+            }
+        }
+        System.out.println(result);
+        /*
+         * Enter your code here. Read input from STDIN. Print output to STDOUT. Your
+         * class should be named Solution.
+         */
+    }
 }
