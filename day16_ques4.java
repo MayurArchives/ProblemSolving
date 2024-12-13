1cb08cff093d7940a9236183c83a91548e6af834
MayurArchives <mayurkanyal10@gmail.com>
2024-12-13 19:33:30 +0530

    HW_Lucky Number 26

--- day16_ques4.java
+++ day16_ques4.java
@@ -1,3 +1,29 @@
+import java.io.*;
+import java.util.*;
+
 public class day16_ques4 {
 
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int arr[] = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        int[] freq = new int[10];
+        for (int i = 0; i < n; i++) {
+            freq[arr[i]]++;
+        }
+        int larg = -1;
+        for (int i = 1; i < 10; i++) {
+            if (freq[i] == i) {
+                larg = i;
+            }
+        }
+        System.out.println(larg);
+        /*
+         * Enter your code here. Read input from STDIN. Print output to STDOUT. Your
+         * class should be named Solution.
+         */
+    }
 }
