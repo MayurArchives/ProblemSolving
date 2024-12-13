5acb8244ba6b9e44000390726ae9a8cc1de76724
MayurArchives <mayurkanyal10@gmail.com>
2024-12-13 19:30:08 +0530

    Print Prefix Sum

--- day16_ques2.java
+++ day16_ques2.java
@@ -1,3 +1,28 @@
+import java.io.*;
+import java.util.*;
+
 public class day16_ques2 {
 
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int arr[] = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+
+        }
+        int prefix[] = new int[n];
+        prefix[0] = arr[0];
+        for (int i = 1; i < n; i++) {
+            prefix[i] = prefix[i - 1] + arr[i];
+        }
+        for (int i = 0; i < n; i++) {
+            System.out.println(prefix[i]);
+
+        }
+        /*
+         * Enter your code here. Read input from STDIN. Print output to STDOUT. Your
+         * class should be named Solution.
+         */
+    }
 }
--- day16_ques5.java
+++
@@ -1,3 +0,0 @@
-public class day16_ques5 {
-
-}
