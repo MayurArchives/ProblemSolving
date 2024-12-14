699122454cbeb322e2944425b57f5ecbe61516a1
MayurArchives <mayurkanyal10@gmail.com>
2024-12-15 00:00:53 +0530

    Print Alternate Row

--- day17_ques3.java
+++ day17_ques3.java
@@ -1,3 +1,28 @@
+import java.io.*;
+import java.util.*;
+
 public class day17_ques3 {
-    
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int m = sc.nextInt();
+        int n = sc.nextInt();
+        int arr[][] = new int[m][n];
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                arr[i][j] = sc.nextInt();
+            }
+        }
+        for (int i = 0; i < m; i += 2) {
+            for (int j = 0; j < n; j++) {
+                System.out.print(arr[i][j] + " ");
+            }
+            System.out.println();
+        }
+
+        /*
+         * Enter your code here. Read input from STDIN. Print output to STDOUT. Your
+         * class should be named Solution.
+         */
+    }
 }
