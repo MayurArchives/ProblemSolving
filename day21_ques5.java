a141a65426dc14b5488366b230b9ff3218c624bb
MayurArchives <mayurkanyal10@gmail.com>
2024-12-19 21:52:09 +0530

    Is Palindrome

--- day21_ques5.java
+++ day21_ques5.java
@@ -1,3 +1,17 @@
-public class day21_ques5 {
+import java.util.Scanner;
 
+public class day21_ques5 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine(); // Input string
+
+        // Reverse the string and check if it equals the original
+        String reversed = new StringBuilder(str).reverse().toString();
+
+        if (str.equals(reversed)) {
+            System.out.println("Palindrome");
+        } else {
+            System.out.println("Not a Palindrome");
+        }
+    }
 }
