a0055037d67df78bc1f07c69c78d498b1960bc57
MayurArchives <mayurkanyal10@gmail.com>
2024-12-19 21:20:06 +0530

    Find Unique

--- day21_ques2.java
+++ day21_ques2.java
@@ -1,3 +1,25 @@
+import java.io.*;
+import java.util.*;
+
 public class day21_ques2 {
 
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+        char ch[] = str.toCharArray();
+        Arrays.sort(ch);
+        int count = 1;
+        for (int i = 1; i < ch.length; i++) {
+            if (ch[i - 1] == ch[i]) {
+                continue;
+            } else {
+                count++;
+            }
+        }
+        System.out.println(count);
+        /*
+         * Enter your code here. Read input from STDIN. Print output to STDOUT. Your
+         * class should be named Solution.
+         */
+    }
 }
