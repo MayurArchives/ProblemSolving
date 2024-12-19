30ae60d6faf2b1a315e7e0f3bb4d1d9743b4e020
MayurArchives <mayurkanyal10@gmail.com>
2024-12-19 21:50:22 +0530

    Find Distance B/W Two Characters

--- day21_ques4.java
+++ day21_ques4.java
@@ -1,3 +1,20 @@
+import java.io.*;
+import java.util.*;
+
 public class day21_ques4 {
 
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+        char ch1 = sc.next().charAt(0);
+        char ch2 = sc.next().charAt(0);
+        int index1 = str.indexOf(ch1);
+        int index2 = str.indexOf(ch2);
+        int dist = Math.abs(index2 - index1) - 1;
+        System.out.println(dist);
+        /*
+         * Enter your code here. Read input from STDIN. Print output to STDOUT. Your
+         * class should be named Solution.
+         */
+    }
 }
