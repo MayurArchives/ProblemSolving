27b80f2d6580f1b1722cc220729828043a2a1e01
MayurArchives <mayurkanyal10@gmail.com>
2024-12-19 21:22:32 +0530

    Locate the Target String

--- day21_ques3.java
+++ day21_ques3.java
@@ -1,3 +1,17 @@
+import java.io.*;
+import java.util.*;
+
 public class day21_ques3 {
 
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+        String target = sc.nextLine();
+        int index = str.indexOf(target);
+        System.out.println(index);
+        /*
+         * Enter your code here. Read input from STDIN. Print output to STDOUT. Your
+         * class should be named Solution.
+         */
+    }
 }
