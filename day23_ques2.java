a075e70f5e26da897a053ed1f26b55a66965757e
MayurArchives <mayurkanyal10@gmail.com>
2024-12-23 22:06:40 +0530

    Target String

--- day23_ques2.java
+++ day23_ques2.java
@@ -1,3 +1,26 @@
-public class day23_ques2 {
+import java.util.*;
 
+public class day23_ques2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String str = sc.nextLine();
+        String target = sc.nextLine();
+
+        // Check if lengths are equal; if not, rotation is impossible
+        if (str.length() != target.length()) {
+            System.out.println("False");
+            return;
+        }
+
+        // Concatenate the original string with itself
+        String doubledStr = str + str;
+
+        // Check if target is a substring of the concatenated string
+        if (doubledStr.contains(target)) {
+            System.out.println("True");
+        } else {
+            System.out.println("False");
+        }
+    }
 }
