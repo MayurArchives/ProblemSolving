a9bd5d5b8d2ac75c0b96507cb899566eb760af1a
MayurArchives <mayurkanyal10@gmail.com>
2024-12-23 22:09:11 +0530

    Sum of All Substrings

--- day23_ques4.java
+++ day23_ques4.java
@@ -1,3 +1,23 @@
+import java.io.*;
+import java.util.*;
+
 public class day23_ques4 {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String str = scanner.nextLine();
+        int sum = 0;
+        for (int i = 0; i < str.length(); i++) {
+            for (int j = i + 1; j <= str.length(); j++) {
+                String sub = str.substring(i, j);
+                sum += Integer.parseInt(sub);
+            }
+        }
+        System.out.println(sum);
+        /*
+         * Enter your code here. Read input from STDIN. Print output to STDOUT. Your
+         * class should be named Solution.
+         */
+    }
 
 }
