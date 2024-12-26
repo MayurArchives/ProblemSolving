ec66dfdf032b5c32e896d16c231494c35272310a
MayurArchives <mayurkanyal10@gmail.com>
2024-12-26 20:16:01 +0530

    Long Pressed Name

--- day24_ques3.java
+++ day24_ques3.java
@@ -1,3 +1,28 @@
+import java.io.*;
+import java.util.*;
+
 public class day24_ques3 {
 
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s1 = sc.nextLine();
+        String s2 = sc.nextLine();
+        int np = 0;
+
+        for (int i = 0; i < s2.length(); i++) {
+            // Corrected comparison
+            if (np < s1.length() && s2.charAt(i) == s1.charAt(np)) {
+                np++;
+            } else if (i > 0 && s2.charAt(i - 1) == s2.charAt(i)) {
+                continue;
+            } else {
+                System.out.print("false");
+                return;
+            }
+        }
+
+        if (np == s1.length()) {
+            System.out.print("true");
+        }
+    }
 }
