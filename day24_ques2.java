36449a28b3e26c674f7700b6631a6444e955b52a
MayurArchives <mayurkanyal10@gmail.com>
2024-12-26 20:14:38 +0530

    First Non-Repeating char

--- day24_ques2.java
+++ day24_ques2.java
@@ -1,3 +1,27 @@
+import java.io.*;
+import java.util.*;
+
 public class day24_ques2 {
 
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+        int freq[] = new int[26];
+        for (int i = 0; i < str.length(); i++) {
+            char ch = str.charAt(i);
+            freq[ch - 'a']++;
+        }
+        for (int i = 0; i < str.length(); i++) {
+            char ch = str.charAt(i);
+            if (freq[ch - 'a'] == 1) {
+                System.out.println(ch); // Print the first non-repeating character
+                return;
+            }
+        }
+        System.out.println(-1);
+        /*
+         * Enter your code here. Read input from STDIN. Print output to STDOUT. Your
+         * class should be named Solution.
+         */
+    }
 }
