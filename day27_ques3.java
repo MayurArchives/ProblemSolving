4b8f7e316d066a94db2004a659e68772b6bbdd46
MayurArchives <mayurkanyal10@gmail.com>
2024-12-29 21:55:14 +0530

    Interchange the Row

--- day27_ques3.java
+++ day27_ques3.java
@@ -1,3 +1,28 @@
-public class day27_ques3 {
+import java.util.Scanner;
 
+public class day27_ques3 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int m = sc.nextInt();
+        int n = sc.nextInt();
+        int[][] matrix = new int[m][n];
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                matrix[i][j] = sc.nextInt();
+            }
+        }
+
+        int[] temp = matrix[0];
+        matrix[0] = matrix[m - 1];
+        matrix[m - 1] = temp;
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
 }
