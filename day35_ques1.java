f984d3a7b4665663cf92350436ccb27b1967a64c
MayurArchives <mayurkanyal10@gmail.com>
2025-01-09 22:12:22 +0530

    sub

---
+++ day35_ques1.java
@@ -0,0 +1,14 @@
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.Set;
+
+public class day35_ques1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // Read the size of the array`
+
+        // The number of unique positive numbers is the answer
+        System.out.println(uniquePositives.size());
+    }
+}
